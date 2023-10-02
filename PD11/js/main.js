ccc4c093694ed9cb80f5061ae0786383526945be
1ekso <chuliganas9@gmail.com>
2023-10-02 13:52:27 +0300

    bitiniai operatoriai :(

---
+++ PD11/js/main.js
@@ -0,0 +1,9 @@
+let bitNumberOne = 10;
+let bitNumberTwo = 20;
+console.log(bitNumberOne & bitNumberTwo); // 0
+console.log(bitNumberOne | bitNumberTwo); // 30
+console.log(~bitNumberTwo); // -21
+console.log(bitNumberOne ^ bitNumberTwo); // 30
+console.log(bitNumberOne << 1); // 20
+console.log(bitNumberOne >> 1); // 5
+console.log(bitNumberOne >>> 1); // 5
