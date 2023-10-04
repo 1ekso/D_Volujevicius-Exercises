5e4e58e87f438886ca7fa61e5959cbbcb93e87d2
1ekso <chuliganas9@gmail.com>
2023-10-04 12:08:21 +0300

    puodukai

---
+++ PD12/js/2.js
@@ -0,0 +1,8 @@
+let puodukai = 7;
+const puodukuMax = 3;
+
+let supakuoti = Math.floor(puodukai / puodukuMax);
+let likePuodukai = puodukai % puodukuMax;
+
+console.log(`supakuotos ${supakuoti} dezes`);
+console.log(`liko ${likePuodukai} puodas/puodeliai`);
