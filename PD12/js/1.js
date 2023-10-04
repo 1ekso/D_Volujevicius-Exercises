70706eaf1fc1c02048026f24789fd26ebc408692
1ekso <chuliganas9@gmail.com>
2023-10-04 11:36:17 +0300

    algoritmai :(

---
+++ PD12/js/1.js
@@ -0,0 +1,9 @@
+let dienos = [5, 6, 4, 5, 4];
+let paskaitos = 0;
+let laikas = 0;
+for (let i = 0; i < dienos.length; i++) {
+    paskaitos += dienos[i];
+    laikas += dienos[i] * 45;
+}
+
+console.log(`prarasi ${laikas} minuciu per savaite del ${paskaitos} pamoku`);
