81688b18b2e3694df01d7f5cc1771a2952355b2a
1ekso <chuliganas9@gmail.com>
2023-10-09 13:40:23 +0300

    salyginiai sakiniai

---
+++ PD13/js/9.js
@@ -0,0 +1,11 @@
+const atvirukasIlg = 8;
+const atvirukasAuk = 5;
+
+const vokasIlg = 8;
+const vokasAuk = 5;
+
+if (atvirukasIlg < vokasIlg && atvirukasAuk < vokasAuk) {
+    console.log("tilps");
+} else {
+    console.log("netilpa");
+}
