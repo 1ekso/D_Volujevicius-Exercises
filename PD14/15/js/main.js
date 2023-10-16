3f5fe6186a4d20c0345221154e52b9519878ff0d
1ekso <chuliganas9@gmail.com>
2023-10-16 14:14:56 +0300

    cikla

---
+++ PD14/15/js/main.js
@@ -0,0 +1,8 @@
+
+
+/*  Laimingi bus bilietai, kurie be
+liekanos dalijasi iš trijų ir kurių bent vienas skaitmuo taip pat dalijasi iš trijų be liekanos
+(skaitmenys negali būti lygūs nuliui). 
+
+Rezultatas
+231 234 237 243*/
