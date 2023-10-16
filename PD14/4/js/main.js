df3d2b90489eef1e7a446780f10113a0df1fd660
Ignas Daunoravicius <Ignas.Daunoravicius@stud.vtmc.lt>
2023-10-16 11:12:24 +0300

    ciklaaaaaaaaaaai

--- PD14/4/js/main.js
+++ PD14/4/js/main.js
@@ -1,6 +1,6 @@
 const Sausis = [-11.5, -10.3, -12, -6.5, -5, -15.5];
 const Vasaris = [-16, -14.5, -11, -10.3, -3.5, 0];
-const Kovas =  [-3, -2.5, 0, 5, 9.5, 11.2];
+const Kovas = [-3, -2.5, 0, 5, 9.5, 11.2];
 let S1 = 0;
 let V1 = 0;
 let K1 = 0;
@@ -18,10 +18,10 @@ for (let i = 0; i < Kovas.length; i++) {
 }
 
 let avgS1 = S1 / Sausis.length;
-let avgV1 = V1 / Sausis.length;
-let avgK1 = K1 / Sausis.length;
+let avgV1 = V1 / Vasaris.length;
+let avgK1 = K1 / Kovas.length;
 
-let bendras = (S1+V1+K1) / (Sausis.length + Vasaris.length + Kovas.length);
+let bendras = (S1 + V1 + K1) / (Sausis.length + Vasaris.length + Kovas.length);
 
 
 console.log(`vidutinio Sausio temp: ${avgS1.toFixed(2)}`);
