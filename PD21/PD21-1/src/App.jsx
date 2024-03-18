f971e3abfa00ec132cd31276541da730ed432866
1ekso <chuliganas9@gmail.com>
2024-03-18 14:33:42 +0200

    nemoku ir nesuprantu kolkas

---
+++ PD21/PD21-1/src/App.jsx
@@ -0,0 +1,16 @@
+import AddTask from "./components/AddTask";
+import Task from "./components/Task"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <AddTask />
+        <Task />
+      </div>
+    </>
+  );
+}
+ 
+export default App;
---
+++ PD21/PD21-1/src/components/AddTask.jsx
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button';
+
+function AddTask() {
+
+    return (
+        <div>
+            <Button variant='contained'> uiii spausk mane!!</Button>
+        </div>
+    );
+  }
+   
+  export default AddTask;
